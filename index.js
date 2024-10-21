abab8f858c0902d4dcb80ca1c848a4c7d17829c7
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-10-21 14:32:58 +0600

    Added env file

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//mongodb connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.uqio6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('readers-cafe-db').collection('users');
+
+    //api requests
+
+    //users
+    app.get("/users", async (req, res)=>{
+        const query = userCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+    })
+
+    app.post("/user", async (req, res)=>{
+        const user = req.body;
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    //categories
+
+    //products
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to Reader's Cafe Database!");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 //import json data
 const books = require("./data/books.json")
